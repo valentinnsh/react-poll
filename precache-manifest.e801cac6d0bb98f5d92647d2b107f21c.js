28331a5d167235363ab3f7f46bd5cbd3ef7366fc
valentinnsh <valentinnsh@gmail.com>
2019-12-22 15:25:35 +0300

    Updates

---
+++ precache-manifest.e801cac6d0bb98f5d92647d2b107f21c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0511a8de73cc50112577a5f9c6da7e46",
+    "url": "/react-poll/index.html"
+  },
+  {
+    "revision": "ef96e59e2e157c5176bd",
+    "url": "/react-poll/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "706f41ffbdf26bf106bf",
+    "url": "/react-poll/static/js/2.39513104.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-poll/static/js/2.39513104.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ef96e59e2e157c5176bd",
+    "url": "/react-poll/static/js/main.0f2e1582.chunk.js"
+  },
+  {
+    "revision": "1aaf11464d3c84950b7d",
+    "url": "/react-poll/static/js/runtime-main.91dc4fdc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-poll/static/media/logo.5d5d9eef.svg"
+  }
+]);
